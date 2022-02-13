45e9578e2eb854775fdf00a74cc5f8046d29ee9c
zuhriatulaini <98894926+zuhriatulaini@users.noreply.github.com>
2022-02-13 23:54:45 +0800

    Update 06-scope.js

---
+++ 06-scope.js
@@ -0,0 +1,41 @@
+/// Soal - 01
+/// Kita sudah belajar mengenai variable scope yang dimiliki oleh Javascript
+/// dan kamu diminta untuk merangkum tentang:
+/// - ada berapa banyak jumlah variable scope pada Javascript?
+/// - jelaskan secara singkat setiap variable scope yang dimiliki oleh Javascript
+/// - buatlah implementasi sederhana dari tiap-tiap variable scope yang dimiliki oleh Javascript
+
+/// WRITE YOUR ANALYSIS HERE
+console.log('SOAL NO 1');
+console.log('ada berapa banyak variable scope pada Javascript?');
+console.log('Jawaban : ada 2 yaitu global dan local');
+console.log('');
+console.log('jelaskan secara singkat setiap variable scope yang dimiliki oleh Javascript');
+console.log('1. Global scope : variabel yang dapat di akses 1 dokument');
+console.log('2. Local scope  : variabel yang hanya dapat di akses pada function yang telah di buat atau block tertentu saja');
+console.log('');
+console.log('buatlah implementasi sederhana dari tiap-tiap variable scope yang dimiliki oleh Javascript');
+console.log('');
+
+const daerah = 'Kabupaten Sumbawa Barat'
+
+function daerahAsal(cek) {
+    console.log('Daerah Tinggal Sekarang' + cek);
+}
+console.log('Kabupaten Sumbawa Barat');
+/// Soal - 02
+/// Kamu diminta untuk melakukan analisa terhadap baris kode dibawah ini
+/// - apa yang akan tampil didalam comsole.log ?
+/// - apa alasan yang menyebabkan hasil dari console itu bukan nilai dari variable name?
+
+/// WRITE YOUR ANALYSIS HERE
+///
+
+
+console.log('SOAL NO 2');
+console.log('apa yang akan tampil didalam console.log?');
+console.log('Jawaban : Mariah');
+console.log('');
+console.log('apa alasan yang menyebabkan hasil dari console itu bukan nilai dari variable name?');
+console.log('Jawaban : karena yang akan di tampilkan ialah parameter dari function printFirstName sedangkan John Watson tidak di tampilkan');
+console.log(''); 
